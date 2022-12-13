5ca82fdbce2b89f98af19e1aa4183109c647c182
Scott Andrew <scott@nwdm.tech>
2022-12-12 18:25:06 -0800

    Initial Commit

---
+++ GoogleFontPreview/GoogleFontPreviewApp.swift
@@ -0,0 +1,17 @@
+//
+//  GoogleFontPreviewApp.swift
+//  GoogleFontPreview
+//
+//  Created by Scott Andrew on 12/12/22.
+//
+
+import SwiftUI
+
+@main
+struct GoogleFontPreviewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
